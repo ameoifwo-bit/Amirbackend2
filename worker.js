27e4c8313c93db8dfe1eb9fc26aacd155a4cc8e0
ameoifwo-bit <ameoifwo@gmail.com>
2025-12-04 02:09:55 +0330

    Create worker.js
    Ok

---
+++ worker.js
@@ -0,0 +1,8 @@
+import { createServer } from ".medusa/server/index.js";
+
+export default {
+  async fetch(request, env, ctx) {
+    const server = createServer();
+    return server(request, env, ctx);
+  }
+};
